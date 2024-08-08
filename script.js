b35592b2aad1243e4bd64f36a125ca9d7171e6c3
NOORI-LIMA <161029810+NOORI-LIMA@users.noreply.github.com>
2024-08-08 09:25:57 -0700

    .....
    .....

---
+++ script.js
@@ -0,0 +1,16 @@
+const container = document.getElementById('container');
+const container = document.getElementById('register');
+const container = document.getElementById('login');
+
+registerbtn.addEventListener('click', () =>{
+    container.classList.add("active");
+});
+
+loginbtn.addEventListener('click', () =>{
+    container.classList.remove("active");
+});
+
+
+
+
+
