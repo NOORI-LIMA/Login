a8f40bb1be870a1ec75557da4dbf46ee3cd1a0a5
Ehsan Ehrari <ehsan.ehrari@codetoinspire.org>
2024-08-12 10:42:44 +0430

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const container = document.getElementById('container');
-const container = document.getElementById('register');
-const container = document.getElementById('login');
+const registerbtn = document.getElementById('register');
+const loginbtn = document.getElementById('login');
 
 registerbtn.addEventListener('click', () =>{
+    alert("Hi");
     container.classList.add("active");
 });
 
 loginbtn.addEventListener('click', () =>{
+    alert("Hi");
+
     container.classList.remove("active");
 });
 
